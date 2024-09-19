8dcb924953f9d9aa849ba9590a9a6342a06791e0
David Park <hyunje.park@mail.utoronto.ca>
2024-09-19 17:23:56 -0400

    edit 1

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -1,19 +1,40 @@
 #### Preamble ####
-# Purpose: Simulates... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Simulates dada
+# Author: David Park 
+# Date: 19 September 2024 
+# Contact: hyunje.park@mail.utoronto.ca
 # License: MIT
 # Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Any other information needed? 
 
 
 #### Workspace setup ####
 library(tidyverse)
-# [...UPDATE THIS...]
+
 
 #### Simulate data ####
-# [...ADD CODE HERE...]
 
+set.seed(304)
+
+start_date <- as.Date("2023-01-01")
+end_date <- as.Date("2023-12-31")
+
+data <-
+  tibble(
+  date =as.Date(runif(n, min= as.numeric(start_date), max= as.numeric(end_date))),
+  number_of_marriages = rpois(n = 100, lambda = 10)
+  )
+
+write_csv(data, file)
+
+package <- show_package("e28bc818-43d5-43f7-b5d9-bdfb4eda5feb")
+resources <- list_package_resources("e28bc818-43d5-43f7-b5d9-bdfb4eda5feb")
+datastore_resources <- filter(resources, tolower(format))
+data <- read_csv("data/raw_data/simulated.csv")
+
+data <- filter(datastore_resources, row_number() == 1 )%>%
+  get_resource()
+
+write_csv(data, "data/raw_data/raw_data.csv")
 
 
--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -1,11 +1,12 @@
 #### Preamble ####
-# Purpose: Downloads and saves the data from [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Downloads and save the data from the data
+# Author: David Park 
+# Date: 19 September 2024 
+# Contact: hyunje.park@mail.utoronto.ca
 # License: MIT
 # Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Any other information needed? 
+
 
 
 #### Workspace setup ####
@@ -16,11 +17,13 @@ library(tidyverse)
 #### Download data ####
 # [...ADD CODE HERE TO DOWNLOAD...]
 
+package <- show_package("e28bc818-43d5-43f7-b5d9-bdfb4eda5feb")
+resources <- list_package_resources("e28bc818-43d5-43f7-b5d9-bdfb4eda5feb")
+datastore_resources <- filter(resources, tolower(format))
+data <- read_csv("data/raw_data/simulated.csv")
 
+data <- filter(datastore_resources, row_number() == 1 )%>%
+  get_resource()
 
-#### Save data ####
-# [...UPDATE THIS...]
-# change the_raw_data to whatever name you assigned when you downloaded it.
-write_csv(the_raw_data, "inputs/data/raw_data.csv") 
-
+write_csv(data, "data/raw_data/raw_data.csv")
          
